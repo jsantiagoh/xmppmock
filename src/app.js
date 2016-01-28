7a89fece3e69360c97fb0b5b8395306cc5b5ade0
Santiago <jsantiagoh@gmail.com>
2016-01-28 13:41:45 +0100

    First version

---
+++ src/app.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const xmpp = require('node-xmpp-server')
+const Component = require('node-xmpp-component')
+var server = null
+
+const COMPONENT_PORT = process.env.COMPONENT_PORT ? process.env.COMPONENT_PORT : 5347
+const COMPONENT_PASS = process.env.COMPONENT_PASS ? process.env.COMPONENT_PASS : 'password'
+
+var startServer = function (done) {
+  server = new xmpp.ComponentServer({
+    port: COMPONENT_PORT
+  })
+
+  server.on('connect', function (component) {
+    component.on('verify-component', function (jid, cb) {
+      console.log(`verify-component '${jid}' on port ${COMPONENT_PORT}, expecting password '${COMPONENT_PASS}'`)
+      return cb(null, COMPONENT_PASS)
+    })
+
+    component.on('online', function () {
+      console.log(`online, ready to receive components at ${COMPONENT_PORT}`)
+    })
+
+    component.on('stanza', (stanza) => {
+      console.log(`[R] ${stanza.root().toString()}`)
+    })
+
+    component.on('disconnect', () => console.log('disconnect'))
+  })
+
+  server.on('listening', done)
+}
+
+startServer(() => console.log("initialization done, happy hacking"))
